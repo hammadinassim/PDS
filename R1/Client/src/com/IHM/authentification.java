8e2d2040cfc66b3dd352fef9a9a1e5f89c0f5366
laura.hollard <laura.hollard@etu.u-pec.fr>
2017-02-13 14:33:10 +0100

    ihm authentification

--- R1/Client/src/com/IHM/authentification.java
+++ R1/Client/src/com/IHM/authentification.java
@@ -3,14 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package IHM;
+package vue;
 
 import static com.sun.java.accessibility.util.AWTEventMonitor.addWindowListener;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
@@ -20,58 +16,43 @@ import javax.swing.border.TitledBorder;
  *
  * @author Laura
  */
-public class authentification extends JFrame {
-
-    public authentification(){
-      /*  WindowListener w = new WindowAdapter(){
-            public void windowClosing(WindowEvent e){
-                System.exit(0);
-            }
-        };
-        addWindowListener(w);*/
-      
-        Toolkit tk = Toolkit.getDefaultToolkit();
-        Dimension dimEcran = tk.getScreenSize();
-        setSize(dimEcran.width, dimEcran.height); 
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        getContentPane().setLayout(new FlowLayout());
-        
-        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        JPanel panneau = new JPanel();
-        panneau.setBorder(new TitledBorder("Authentifiez vous"));
-
-        getContentPane().setLayout(new FlowLayout());
-        getContentPane().add(panneau);
-        
-        JLabel profil = new JLabel();
-        profil.setText("Vous êtes :");
-        panneau.add(profil);
-        ButtonGroup groupe = new ButtonGroup();
-        JCheckBox bouton1 = new JCheckBox("Manager");
-        groupe.add(bouton1);
-        panneau.add(bouton1);
-        JCheckBox bouton2 = new JCheckBox("Réparateur");
-        groupe.add(bouton2);
-        panneau.add(bouton2);
-        System.getProperty("line.separator");
+public class Authentification extends JPanel {
+private JCheckBox c1, c2;
+    public Authentification(){
+    
+    
+        JPanel pannel = new JPanel();
+      //  setSize(400,400);
+        setLocation(100, 100);
+        add(pannel);
+        pannel.setBorder(new TitledBorder("Authentifiez vous")); 
+        pannel.setLayout(new BoxLayout(pannel, BoxLayout.Y_AXIS)); 
+     
+        JLabel id = new JLabel();
+        id.setText("Identifiant :");
+        JTextField enterID = new JTextField("Entrez votre identifiant", 20);
+        pannel.add(id);
+        pannel.add(enterID);
         JLabel mdp = new JLabel();
         mdp.setText("Mot de passe :");
-        JTextField entre = new JTextField("Entrez votre mot de passe", 20);
-        panneau.add(mdp);
-        panneau.add(entre);
-        setContentPane(panneau);
+        JTextField enterPWD = new JTextField("Entrez votre mot de passe", 20);
+        pannel.add(mdp);
+        pannel.add(enterPWD);
         setVisible(true);
+     
+    }
+
+}
         
-    }   
+   
+        
+        
+    
     
  
     
     
     
-    public static void main(String[] args) {
-       JFrame frame = new authentification();
-    }
+  
     
 
-}
